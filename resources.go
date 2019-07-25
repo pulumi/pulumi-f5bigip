7cbdd581b22fc9bacc95221d292a7f29f7d29927
stack72 <public@paulstack.co.uk>
2019-07-25 10:49:31 -0700

    Run tfgen with latest pulumi-terraform

--- resources.go
+++ resources.go
@@ -98,7 +98,7 @@ func Provider() tfbridge.ProviderInfo {
 		},
 		JavaScript: &tfbridge.JavaScriptInfo{
 			Dependencies: map[string]string{
-				"@pulumi/pulumi": "^0.17.0",
+				"@pulumi/pulumi": "^0.17.25",
 			},
 			DevDependencies: map[string]string{
 				"@types/node": "^8.0.25", // so we can access strongly typed node definitions.
