bdc185ea843a86adb7874318d8d60d0f3be95d06
Ian Wahbe <ian@wahbe.com>
2024-01-19 11:56:19 -0800

    format test
    I have checked and the test compiles fine (failing in CI). I'm going to see if there was a
    version weirdness that has resolved.

--- examples/virtualappliance/index.ts
+++ examples/virtualappliance/index.ts
@@ -5,40 +5,40 @@ import * as backendinstances from "./loadbalancedinstances";
 import * as https from "https";
 import fetch from "node-fetch";
 
-function sleep(ms: number) : Promise<void> {
-    return new Promise(resolve => setTimeout(resolve, ms));
+function sleep(ms: number): Promise<void> {
+    return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 // Wait for the virtual appliance to be avaiabe to log in to.
-const virtualapplicanceAddress = pulumi.all([virtualappliance.f5Address, virtualappliance.f5Password])
-        .apply(async ([address,password]) => {
-    let ready = false;
-    const agent = new https.Agent({
-        rejectUnauthorized: false
-    });
-    const token = Buffer.from(`admin:${password}`).toString("base64");
-    while (!ready) {
-        try {
-            const res = await fetch(`${address}/mgmt/tm/ltm`, { 
-                agent, 
-                headers: { 
-                    Authorization: `Basic ${token}`,
-                },
-            });
-            if (res.status == 200) {
-                console.log("bigip virtual appliance is ready")
-                ready = true;
-             }
-
-        } catch (err) {
-            console.log(`caught exception: ${err}`)
+const virtualapplicanceAddress = pulumi
+    .all([virtualappliance.f5Address, virtualappliance.f5Password])
+    .apply(async ([address, password]) => {
+        let ready = false;
+        const agent = new https.Agent({
+            rejectUnauthorized: false,
+        });
+        const token = Buffer.from(`admin:${password}`).toString("base64");
+        while (!ready) {
+            try {
+                const res = await fetch(`${address}/mgmt/tm/ltm`, {
+                    agent,
+                    headers: {
+                        Authorization: `Basic ${token}`,
+                    },
+                });
+                if (res.status == 200) {
+                    console.log("bigip virtual appliance is ready");
+                    ready = true;
+                }
+            } catch (err) {
+                console.log(`caught exception: ${err}`);
+            }
+            // Keep trying
+            console.log("waiting for bigip virtual appliance address to be ready");
+            sleep(10000);
         }
-        // Keep trying
-        console.log("waiting for bigip virtual appliance address to be ready")
-        sleep(10000);
-    }
-    return address;
-});
+        return address;
+    });
 
 const f5bigipProvider = new f5bigip.Provider("bigipProvider", {
     address: virtualapplicanceAddress,
@@ -46,42 +46,60 @@ const f5bigipProvider = new f5bigip.Provider("bigipProvider", {
     username: "admin",
 });
 
-const monitor = new f5bigip.ltm.Monitor("backend", {
-    name: "/Common/backend",
-    parent: "/Common/http",
-    send: "GET /\r\n",
-    timeout: 5,
-    interval: 10,
-}, { provider: f5bigipProvider });
+const monitor = new f5bigip.ltm.Monitor(
+    "backend",
+    {
+        name: "/Common/backend",
+        parent: "/Common/http",
+        send: "GET /\r\n",
+        timeout: 5,
+        interval: 10,
+    },
+    { provider: f5bigipProvider },
+);
 
-const pool = new f5bigip.ltm.Pool("backend", {
-    name: "/Common/backend",
-    monitors: [monitor.name],
-    allowNat: "yes",
-    allowSnat: "yes",
-}, { provider: f5bigipProvider });
+const pool = new f5bigip.ltm.Pool(
+    "backend",
+    {
+        name: "/Common/backend",
+        monitors: [monitor.name],
+        allowNat: "yes",
+        allowSnat: "yes",
+    },
+    { provider: f5bigipProvider },
+);
 
 backendinstances.instancePublicIps.map((backendAddress, i) => {
+    const node = new f5bigip.ltm.Node(
+        `backend-${i}`,
+        {
+            name: `/Common/node-${i}`,
+            address: backendAddress.apply((x) => x.toString()),
+        },
+        { provider: f5bigipProvider },
+    );
 
-    const node = new f5bigip.ltm.Node(`backend-${i}`, {
-        name: `/Common/node-${i}`,
-        address: backendAddress.apply(x=>x.toString()),
-    }, { provider: f5bigipProvider })
-
-    const applicationPoolAttachment = new f5bigip.ltm.PoolAttachment(`backend-${i}`, {
-        pool: pool.name,
-        node: node.name.apply( x => x + ":80"),
-    }, { provider: f5bigipProvider });
-
+    new f5bigip.ltm.PoolAttachment(
+        `backend-${i}`,
+        {
+            pool: pool.name,
+            node: node.name.apply((x: string) => x + ":80"),
+        },
+        { provider: f5bigipProvider },
+    );
 });
 
-const virtualServer = new f5bigip.ltm.VirtualServer("backend", {
-    pool: pool.name,
-    name: "/Common/backend",
-    destination: virtualappliance.f5PrivateIp,
-    port: 80,
-    sourceAddressTranslation: "automap",
-}, { provider: f5bigipProvider, dependsOn: [pool] });
+new f5bigip.ltm.VirtualServer(
+    "backend",
+    {
+        pool: pool.name,
+        name: "/Common/backend",
+        destination: virtualappliance.f5PrivateIp,
+        port: 80,
+        sourceAddressTranslation: "automap",
+    },
+    { provider: f5bigipProvider, dependsOn: [pool] },
+);
 
 export let instanceIp = virtualappliance.f5Address;
 export let password = virtualappliance.f5Password;
